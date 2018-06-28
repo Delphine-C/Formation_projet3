65c29c6e081d97c24e826349e630dc584abbe46f
Delphine-C <corneil.delphine@gmail.com>
2018-06-28 10:19:56 +0200

    Create homepage

--- view/frontend/home.php
+++ view/frontend/home.php
@@ -9,15 +9,19 @@ $title='Accueil';
 
 ob_start(); ?>
 <div class="container">
+    <header>
+        <h2>Voici les derniers chapitres publiés</h2>
+    </header>
     <div class="row">
 <?php
         while($chapter=$chapters->fetch())
         {?>
         <div class="3u">
             <section>
-                <a href="#" class="image full"><img src="images/pics01.jpg" alt="" /></a>
+                <a href="#" class="image full"><img src="web/images/alaska-ice-mountains.jpg" alt="skiers" /></a>
+                <h3>Chapitre <?= $chapter['num'] ?></h3>
                 <p><?= $chapter['title'] ?></p>
-                <a href="?chapter=<?=  $chapter['id'] ?>" class="button">Lire la suite</a>
+                <a href="?chapter=<?=  $chapter['id'] ?>" class="button">Lire le chapitre</a>
             </section>
         </div>
         <?php } ?>
