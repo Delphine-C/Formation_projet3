671d8ecf27939263d2052ecc15b79dd9e455e601
Delphine-C <corneil.delphine@gmail.com>
2018-07-02 16:48:46 +0200

    Modify index.php : call the router

--- index.php
+++ index.php
@@ -5,3 +5,13 @@
  * Date: 18/06/2018
  * Time: 11:47
  */
+session_start();
+
+require_once ('Router.php');
+
+$_SESSION['arg']=explode("=",$_GET['r'],2);
+
+$request= (!empty($_GET['r'])) ? $_SESSION['arg'][0] : 'accueil';
+
+$router=new Router($request);
+$router->findController();
